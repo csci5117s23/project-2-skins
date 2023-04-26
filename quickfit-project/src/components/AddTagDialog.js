ad770fbd533734c056c11703904e17a04c2694f0
Joshua Yang <quickchicken02@gmail.com>
2023-04-25 22:51:17 -0500

    component work

--- quickfit-project/src/components/AddTagDialog.js
+++ quickfit-project/src/components/AddTagDialog.js
@@ -1,26 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
 // MUI Imports
-import { 
-    Autocomplete,
-    Button,
-    Chip,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-    IconButton,
-    TextField,
-    Tooltip, 
-    Typography 
-} from '@mui/material';
+import {
+  Autocomplete,
+  Button,
+  Chip,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 // MUI Icons
-import AddIcon from '@mui/icons-material/Add';
+import AddIcon from "@mui/icons-material/Add";
+import AddTagAutocomplete from "./AddTagAutocomplete";
 
 export default function AddTagDialog() {
-  
-  // --------------------------------------------------------------------
-  // --- Form popup -----------------------------------------------------
+
+  // --- Form popup state hooks & functions ------------------------------
   // State hook to keep track of whether or not add tag popup should show
   const [open, setOpen] = React.useState(false);
 
@@ -32,66 +32,26 @@ export default function AddTagDialog() {
   const handleClose = () => {
     setOpen(false);
   };
-  // --------------------------------------------------------------------
-  // --------------------------------------------------------------------
-
-  // S
 
   return (
     <div>
       {/* Add tag button */}
       <Tooltip title="Add a tag">
-        <Button 
-            variant="contained" 
-            onClick={handleClickOpen}
-            sx={{
-                // mt: '75%'
-            }}>
-            <AddIcon/>
+        <Button variant="contained" onClick={handleClickOpen}>
+          <AddIcon />
         </Button>
       </Tooltip>
 
       {/* Add tag popup */}
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle
-        >Add tags</DialogTitle>
+        <DialogTitle>Add tags</DialogTitle>
         <DialogContent>
           <DialogContentText>
             Tags created here will be added to your list of personal tags.
           </DialogContentText>
-
-            {/* List of user tag inputs */}
-            <Autocomplete
-              multiple
-              options={[]}
-              freeSolo
-              renderTags={(value, getTagProps) =>
-                value.map((option, index) => (
-                  <Chip
-                    variant="outlined"
-                    label={option}
-                    {...getTagProps({ index })}
-                  />
-                ))
-              }
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  autoFocus
-                  fullWidth
-                  variant="outlined"
-                  label="Type 'enter' to confirm a tag"
-                  placeholder="Press 'enter' to confirm a tag"
-                  sx={{
-                    mt: 2
-                  }}
-                />
-              )}
-            />
-
-
+          <AddTagAutocomplete />
         </DialogContent>
-        
+
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
           <Button onClick={handleClose}>Add</Button>
