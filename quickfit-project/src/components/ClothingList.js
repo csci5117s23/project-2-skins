27204fd73f0be69f55b9fdf5f2b2a8dce4c69cc2
Joshua Yang <quickchicken02@gmail.com>
2023-05-01 00:15:45 -0500

    remove unecessary imports

--- quickfit-project/src/components/ClothingList.js
+++ quickfit-project/src/components/ClothingList.js
@@ -1,31 +1,9 @@
 import React, { useState, useEffect } from "react";
-// import styles from '@/styles/Home.module.css'
-import Head from "next/head"; // Next-js imports
-import Image from "next/image";
-import { Inter } from "next/font/google";
+// MUI Component imports
 import {
-  ClerkProvider,
-  SignUp,
-  SignIn,
-  SignedIn,
-  SignedOut,
-} from "@clerk/clerk-react"; // Clerk authorization imports
-
-import {
-  AppBar,
   Box,
-  Button,
-  Card,
-  CardActions,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  Typography,
-  Stack,
-  Grid,
-  TextField,
-  styled,
 } from "@mui/material";
+// Custom Component imports
 import ClothingCard from "@/components/ClothingCard";
 
 export default function ClothingList(props) {
